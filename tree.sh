b4dce1a15de7cd6defc4e01add64c1999ef4fb7e
bard678 <bard76217@gmail.com>
2025-03-28 03:40:02 -0700

    Main

--- tree.sh
+++
@@ -1,4 +0,0 @@
-mkdir social-media-server
-cd social-media-server
-mkdir src, src\config, src\controllers, src\models, src\routes, src\middleware, src\services, src\utils, src\uploads, src\sockets
-New-Item .env, package.json, README.md, server.js, src\app.js -ItemType File
